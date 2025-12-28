d95a73b37281149f61d89daa9aeefd414e777d3d
Abdullah Addoun <addoun.abdellah@gmail.com>
2025-12-28 15:43:10 +0100

    component(hyperui-accordion): adding initial files of the angular library

---
+++ libs/angular/hyperui/accordion/src/lib/accordion/accordion.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-accordion',
+  imports: [],
+  templateUrl: './accordion.html',
+  styleUrl: './accordion.css',
+})
+export class Accordion {}
