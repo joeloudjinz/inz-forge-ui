85be19b72f5c866ef58bf7401feeb188bff55351
Abdullah Addoun <addoun.abdellah@gmail.com>
2025-12-28 15:34:12 +0100

    feat(global): adding global base configuration for tailwindcss

---
+++ tailwind.config.base.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        inz: {
+          primary: 'var(--inz-primary, #3b82f6)',
+          secondary: 'var(--inz-secondary, #1e293b)',
+        }
+      },
+      borderRadius: {'inz-md': 'var(--inz-radius, 0.5rem)'}
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
+};
